a1b8f7274f15169987e2ace0d107b0dd8db1a09f
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-08 20:06:10 +0600

    Add files via upload

--- final.php
+++ final.php
@@ -1,16 +1,16 @@
 <?php
+
 class Animal{
-  final public function sound(){
-    return "Animal Sound";
-  }
-  
+    final public function sound(){
+        return "Animal Sound";
+    }
 }
+
 class Dog extends Animal{
-  public function sound(){
-    return "Dog Barks";
-  }
 }
-$obj=new Dog();
-$output=$obj->sound();
+
+$obj = new Dog();
+$output = $obj->sound();
+
 echo $output;
 ?>
